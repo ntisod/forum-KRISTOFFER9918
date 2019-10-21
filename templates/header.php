097bfa45226d1670536cb521e3cff9683b41babd
KRISTOFFER9918 <43605423+KRISTOFFER9918@users.noreply.github.com>
2019-10-21 12:21:43 +0200

    .

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!--Header-->
+<header class="w3-row w3-purple">
+    <!--Header logo + name-->
+    <a href="." class="w3-col s3 m3 l3">
+        <img src="nti-logo-black.png" alt="Black NTI logo" style="width: 100px" >
+    </a>
+    <h1 class="w3-col s9 m9 l9">NTI Forum</h1>
+</header>
+
+<nav>
+    <!--Header menu-->
+    <ul class="w3-bar w3-border w3-light-grey">
+        <li class="w3-bar-item w3-button w3-mobile"><a href="" >Nytt inlägg</a></li>
+        <li class="w3-bar-item w3-button w3-mobile"><a href="" >Mina inlägg</a></li>
+        <li class="w3-bar-item w3-button w3-mobile"><a href="" >Följande</a></li>
+        <li class="w3-bar-item w3-button w3-mobile"><a href="login.php">Logga in</a></li>
+        <li class="w3-bar-item w3-button w3-mobile"><a href="" >Profil</a></li>
+    </ul>
+</nav>
